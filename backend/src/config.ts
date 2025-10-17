4247bc3d76f63606c7a8c3853f29fa6763fcc249
Dainius Genzuras <dainiusgenzuras02@gmail.com>
2025-10-17 14:31:33 +0300

    Updated comments in the config file

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,7 +1,7 @@
 // src/config.ts
 export const JWT_SECRET =
-  process.env.JWT_SECRET ?? '⚠️  never commit this default to git';
-export const ACCESS_TOKEN_TTL = '1h';          // or "900s", "24h", etc.
+  process.env.JWT_SECRET ?? 'never commit this default to git';
+export const ACCESS_TOKEN_TTL = '1h';
 export const COOKIE_OPTIONS = {
   httpOnly: true,
   sameSite: 'strict' as const,
